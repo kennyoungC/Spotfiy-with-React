ecc65c2a077eb3524d35318b8e6339edccbff399
Kenneth <obikenneth913@gmail.com>
2022-04-29 21:32:25 +0300

    updated

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import logo from "./logo.svg"
 import "./App.css"
 import LoginPage from "./Components/LoginPage"
-
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap-icons/font/bootstrap-icons.css"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Home from "./Components/Home"
 function App() {
   return (
-    <div className="App">
-      <LoginPage />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/home" element={<Home />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   )
 }
 
