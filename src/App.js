c3b953faecb9c47594d8d701d0bb215f68b4cb49
Kenneth <obikenneth913@gmail.com>
2022-04-30 19:05:27 +0300

    appjs updated

--- src/App.js
+++ src/App.js
@@ -5,14 +5,24 @@ import "bootstrap-icons/font/bootstrap-icons.css"
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Home from "./Components/Home"
 import AlbumPage from "./Components/AlbumPage"
+import ArtistPage from "./Components/ArtistPage"
+import { useState } from "react"
 function App() {
+  const [name, setName] = useState("")
+  const handleNameInput = (e) => {
+    setName(e.target.value)
+  }
   return (
     <BrowserRouter>
       <div className="App">
         <Routes>
-          <Route path="/" element={<LoginPage />} />
-          <Route path="/home" element={<Home />} />
+          <Route
+            path="/"
+            element={<LoginPage name={name} funcName={handleNameInput} />}
+          />
+          <Route path="/home" element={<Home userName={name} />} />
           <Route path="/album/:albumId" element={<AlbumPage />} />
+          <Route path="/artist/:artistId" element={<ArtistPage />} />
         </Routes>
       </div>
     </BrowserRouter>
